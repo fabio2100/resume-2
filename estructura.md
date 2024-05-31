19cf262c25713c61145c259e395d923b68bda0a9
Fabio Squizzato <fsquizzato@visualmedica.com>
2024-05-31 15:44:45 -0300

    scrolling bars

---
+++ estructura.md
@@ -0,0 +1,14 @@
+info general
+    acerca
+    datos
+perfiles -> card
+habilidades
+    hard
+        conceptos -> card
+        tecnologias -> card
+    soft -> card
+idiomas -> card
+cursos  -> card
+experiencia -> card
+educacion   -> card
+portfolio   -> card
---
+++ src/app/components/MainCard.js
@@ -0,0 +1,9 @@
+export default function MainCard(){
+
+    return (
+        <div className="card">
+            <h3>title</h3>
+            <p>Lorem ipsum is typically a corrupted version of De finibus bonorum et malorum, a 1st-century BC text by the Roman statesman and philosopher Cicero, with words altered, added, and removed to make it nonsensical and improper Latin. The first two words themselves are a truncation of dolorem ipsum ("pain itself").</p>
+        </div>
+    )
+}
