55d069f308a06a2557c91922453f94bf2a01746c
Fabio Squizzato <fsquizzato@visualmedica.com>
2024-06-05 17:56:09 -0300

    i18

--- src/app/page.js
+++ src/app/page.js
@@ -2,11 +2,21 @@
 import styles from "./page.module.css";
 import MainCard from "./components/MainCard";
 import CardDisplayer from "./components/CardDisplayer";
+import { useTranslation } from "react-i18next";
+import "../config/i18next.config"
 
 export default function Home() {
 
+
+  const {t, i18n} = useTranslation(["info"])
+  const changeLanguage = ( ) =>{
+    i18n.changeLanguage("es")
+  }
+
   return (
     <main className={styles.main}>
+      <div>{t('title')}</div>
+      <button onClick={changeLanguage}>Change language</button>
       <CardDisplayer className="cardDisplayer">
         <MainCard />
         <MainCard />
